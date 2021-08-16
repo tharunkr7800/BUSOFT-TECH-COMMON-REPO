81e4282cadf909c5b76c0a198d7fb397524e8187
diviyaseshani03 <77004097+diviyaseshani03@users.noreply.github.com>
2021-08-16 15:47:25 +0530

    Delete UI-LOOPBACK/loopback directory

--- UI-LOOPBACK/loopback/controllers/index.ts
+++
@@ -1,9 +0,0 @@
-// export * from './admin.controller';
-export * from './contacts.controller';
-export * from './ping.controller';
-export * from './todo-controller.controller';
-// export * from './user.controller';
-export * from './users.controller';
-// export * from './incident-test.controller';
-// export * from './usertest.controller';
-
